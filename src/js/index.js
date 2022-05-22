f7304f18130054e1f52e1e1a17c42be68cfbe885
Tumee <tumenchodor@gmail.com>
2022-05-22 17:56:51 +0800

    add state and control functions

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,31 @@
+require("@babel/polyfill");
 import Search from "./model/search";
 
-let search = new Search("pasta");
-search.doSearch().then((r) => console.log(r));
+/**
+ * Web App төлөв
+ * -Хайлтын query, үр дүн
+ * -Тухайи үзүүлж буй жор
+ * -Liked жорууд
+ * -Захиалгын жорын найрлаганууд
+ */
+
+const state = {};
+
+const controlSearch = async() => {
+    // 1) Get input from user
+    const query = "pizza";
+    if (query) {
+        // 2) Create new search object
+        state.search = new Search(query);
+        // 3) UI section
+        // 4) DoSearch
+        await state.search.doSearch();
+        // 5) Display result
+        console.log(state.search.result);
+    }
+};
+
+document.querySelector(".search").addEventListener("submit", (e) => {
+    e.preventDefault();
+    controlSearch();
+});
