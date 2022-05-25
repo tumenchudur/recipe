cff8532e0832f22d4fd195df360806d97928f09f
Tumee <tumenchodor@gmail.com>
2022-05-25 17:16:38 +0800

    set the oparation mode and cleaned some files

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = {
     devServer: {
         static: "./docs",
     },
-    devtool: "inline-source-map",
+    // devtool: "inline-source-map",
     plugins: [
         new HtmlWebpackPlugin({
             // Also generate a test.html
